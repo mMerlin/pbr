c5a22974a23539962ddec14422fb160d4d779179
H. Phil Duby <philduby@phriendly.net>
2018-03-07 23:17:51 -0700

    delete and ignore generated files

--- storage/framework/views/dbc2d1778a37717c5ba8539828e34e84d37c971a.php
+++
@@ -1,6 +0,0 @@
-<div class="test_partial">
- Name: <b><?php echo e($bioreactor->name); ?></b><br>City: <b><?php echo e($bioreactor->city); ?></b><br>Country: <b><?php echo e($bioreactor->country); ?></b><br>
-<?php if( isset($show_map)): ?>
-found show_map variable
-<?php endif; ?>
-</div>
